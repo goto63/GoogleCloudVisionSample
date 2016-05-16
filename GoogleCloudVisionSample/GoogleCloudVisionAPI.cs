9bb8175c94c62f3efe7035046e1c44e57078f0e1
goto63 <tgoto63@gmail.com>
2016-05-16 23:27:40 +0700

    Add google cloud vision sample.

---
+++ GoogleCloudVisionSample/GoogleCloudVisionAPI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Apis.Services;
+using Google.Apis.Vision.v1;
+using Google.Apis.Vision.v1.Data;
+
+namespace GoogleCloudVisionSample
+{
+    public class GoogleCloudVisionAPI
+    {
+        private string apiKey;
+
+        public GoogleCloudVisionAPI(string apiKey)
+        {
+            this.apiKey = apiKey;
+        }
+
+        public async Task<BatchAnnotateImagesResponse> RequestAnotate(string base64Img)
+        {
+            var service = new VisionService(new BaseClientService.Initializer
+            {
+                ApiKey = apiKey
+            });
+
+            var request = service.Images.Annotate(new BatchAnnotateImagesRequest
+            {
+                Requests = new[]
+                {
+                    new AnnotateImageRequest
+                    {
+                        Image = new Image
+                        {
+                            Content = base64Img
+                        },
+                        Features = new[]
+                        {
+                            "TEXT_DETECTION"
+                        }.Select(x=> new Feature { Type = x })
+                         .ToArray()
+                    }
+                }
+            });
+
+            return await request.ExecuteAsync();
+        }
+    }
+}
+
--- GoogleCloudVisionSample/Program.cs
+++ GoogleCloudVisionSample/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace GoogleCloudVisionSample
 {
@@ -6,7 +8,30 @@ namespace GoogleCloudVisionSample
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var apiKey = args[0];
+            var gcv = new GoogleCloudVisionAPI(apiKey);
+            var response = gcv.RequestAnotate(readImage());
+
+            var resJSON = JsonConvert.SerializeObject(response.Result.Responses, Formatting.Indented);
+            Console.WriteLine("Responses ==================================================");
+            Console.WriteLine(resJSON);
+            Console.WriteLine("==================================================");
+        }
+
+        /// <summary>
+        /// Get base64 image.
+        /// </summary>
+        public static string readImage()
+        {
+            byte[] bs = null;
+
+            using (var fs = File.Open("./image_in.jpg", FileMode.Open))
+            {
+                bs = new byte[fs.Length];
+                fs.Read(bs, 0, bs.Length);
+            }
+
+            return Convert.ToBase64String(bs);
         }
     }
 }
